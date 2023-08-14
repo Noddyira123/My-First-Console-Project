24825991065138fea9592de831b2d0fac373f7eb
Noddyira123 <96566372+Noddyira123@users.noreply.github.com>
2023-08-14 17:57:08 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+console.log("My first console project");
+var firstName = "Harry";
+var secondName = "Potter";
+
+var fullname = firstName.concat(secondName);
+console.log(fullname);
+
+var a = 11;
+var b = 13;
+
+var add = a+b;
+console.log(add);
+
+
+var subtract = a-b;
+console.log(subtract);
+
+
+
+var multiply = a*b;
+console.log(multiply);
+
+
+
+var divide = a/b;
+console.log(divide);
